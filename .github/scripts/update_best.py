6cbdfe47b8a1e32e361014658c295a630267dad6
DenatYeti <fdam39@gmail.com>
2025-04-29 12:46:47 +0200

    testing

--- .github/scripts/update_best.py
+++ .github/scripts/update_best.py
@@ -1,5 +1,6 @@
 import os
 import csv
+from datetime import datetime
 
 BEST_CSV = os.path.join("..","..","coloring","Resources","best.csv")
 ALGOS_ROOT = os.path.join("..","..","Algos")
@@ -10,21 +11,21 @@ def read_solution_file(filename):
 
 def load_best_solutions():
     best_solutions = {}
-    if not os.path.exists("../../coloring/Resources/best.csv"):
+    if not os.path.exists(BEST_CSV):
         return best_solutions
     
     with open(BEST_CSV, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             instance = row['Instance']
-            best = int(row['best'])
-            algo = row.get('algorithm', 'Unknown')
-            best_solutions[instance] = (best,algo)
+            best = int(row['Best'])
+            algo = row.get('Algorithm', 'Unknown')
+            old_timestamp = row.get('Last Improved', 'Unknown')
+            best_solutions[instance] = (best,algo, old_timestamp)
     return best_solutions
 
 def update_best_solutions():
     best_solutions = load_best_solutions()
-    algos = {}
 
     for root, _, files in os.walk(ALGOS_ROOT):
         algorithm = os.path.basename(root)
@@ -40,18 +41,19 @@ def update_best_solutions():
             old_best, old_algo = best_solutions.get(instance, (float('inf'), 'Unknown'))
             # Should only replace if strictly better.
             if new_bound < old_best:
-                best_solutions[instance] = (new_bound,algorithm)
+                best_solutions[instance] = (new_bound,algorithm, datetime.now().isoformat())
     
     with open(BEST_CSV, "w", newline='') as csvfile:
-        fieldnames = ['Instance', 'best', 'algorithm']
+        fieldnames = ['Instance', 'Best', 'Algorithm', 'Last Improved']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         writer.writeheader()
-        for instance, (best,algo) in sorted(best_solutions.items()):
+        for instance, (best, algo, ts) in sorted(best_solutions.items()):
             writer.writerow({
                 'Instance': instance, 
-                'best': best, 
-                'algorithm': algorithm
+                'Best': best, 
+                'Algorithm': algo,
+                'Last Improved': ts
             })
 
 if __name__ == "__main__":
